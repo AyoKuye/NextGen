e1c43930274700a6cdcfdf070ea31e435f910ebb
Prakash-sa <sainiprakash525@gmail.com>
2023-04-01 12:56:58 -0500

    made good ui

--- frontend/src/components/CreateProject.js
+++ frontend/src/components/CreateProject.js
@@ -62,21 +62,21 @@ function CreateProject({project}) {
 
   return (
     <div className='bg-white flex justify-center flex-col align-middle content-cente m-10 w-fit'>
-        <div className="border-black border-2 border-solid m-2 flex flex-row items-center align-middle bg-gray-100 w-700">
+        <div className="border-black border-2 border-solid m-2 p-5 flex flex-row items-center align-middle bg-gray-100 w-700">
         <div>
           <div className="px-2">Project ID: </div>
           <div>
             <input type="text"  id="qty" className='w-22 py-1 px-1 m-1 border-solid border-2 border-black' placeholder="Project ID" onChange={(e)=>setProjectID(e.target.value)}/>
           </div>
         </div>
-        <div>
+        <div className='mx-5'>
           <div className="px-2">Project Name: </div>
           <div>
             <input type="text"  id="qty" className='w-22 py-1 px-1 m-1 border-solid border-2 border-black' placeholder="Project Name" onChange={(e)=>setProjectName(e.target.value)}/>
           </div>
         </div>
         <div>
-          <button id="join-leave" className = "bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded text-white m-2" onClick={()=>createProject()}>Create Project</button> 
+          <button id="join-leave" className = "bg-blue-500 hover:bg-blue-700 py-2 px-4 mx-5 rounded text-white m-2" onClick={()=>createProject()}>Create Project</button> 
         </div>
       </div>
     </div>
