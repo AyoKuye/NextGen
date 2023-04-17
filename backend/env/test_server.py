319c74e584e62a4446da740105edea856f5d0b19
Ayo Kuye <ayokuye@gmail.com>
2023-04-17 17:05:19 -0500

    test for creating project

--- backend/env/test_server.py
+++ backend/env/test_server.py
@@ -15,3 +15,29 @@ def test_encrption_decryption():
     decryptedpassword = base.decrypt(encryptedpassword, s, d)
 
     assert (decrypteduserid == userID) & (decryptedpassword == password)
+
+
+
+def test_create_project():
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    test_project_data = {
+        "projectName": "Test Project",
+        "projectID": "testProject123",
+        "user": "testUser"
+    }
+
+    with client:
+        response = client.post('/create_project_endpoint', json=test_project_data)
+        data = json.loads(response.data)
+        
+        # Check if the project creation was successful
+        assert data["data"] == "success"
+
+        # Now, try to create the same project again to check if it fails as expected
+        response = client.post('/create_project_endpoint', json=test_project_data)
+        data = json.loads(response.data)
+
+        # Check if the project creation failed as expected
+        assert data["data"] == "failure"
